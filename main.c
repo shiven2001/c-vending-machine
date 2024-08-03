6b115af747200acc251f7cd785709b07ce3e7a9b
Shiven Garg <61319361+shiven2001@users.noreply.github.com>
2024-08-04 01:37:09 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,3 @@
-/**
-* ENGG1110 Problem Solving by Programming
-*
-* Course Project
-*
-* I declare that the project here submitted is original
-* except for source material explicitly acknowledged,
-* and that the same or closely related material has not been
-* previously submitted for another course.
-* I also acknowledge that I am aware of University policy and
-* regulations on honesty in academic work, and of the disciplinary
-* guidelines and procedures applicable to breaches of such
-* policy and regulations, as contained in the website.
-*
-* University Guideline on Academic Honesty:
-* https://www.cuhk.edu.hk/policy/academichonesty/
-*
-* Student Name : SHIVEN GARG
-* Student ID : 1155138055
-* Class/Section : ENGG1110D
-* Date : 11/08/2019
-*/
-
-
 #include <stdio.h>
 int static ans4;
 int static ans1;
